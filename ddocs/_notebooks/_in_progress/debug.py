1e498df4d714269f973c9d19e7c994b99ae20fe9
Kemal Inecik <benkemalim@gmail.com>
2025-05-27 20:50:33 +0200

    Update

--- ddocs/_notebooks/_in_progress/debug.py
+++ ddocs/_notebooks/_in_progress/debug.py
@@ -1,13 +1,14 @@
 import sys
+
 sys.path.append("/Users/kemalinecik/git_nosync/idtrack")
 
 import os
-import time
 import pickle
+import time
 
 import idtrack
 
-local_dir = "/Users/kemalinecik/Downloads/idtrack_temp_2"
+local_dir = "/Users/kemalinecik/Downloads/idtrack_temp"
 idt = idtrack.API(local_repository=local_dir)
 idt.configure_logger()
 # dm = idt.get_database_manager(organism_name='homo_sapiens', last_ensembl_release=114)
@@ -15,5 +16,4 @@ idt.configure_logger()
 # df=dm.get_db(df_indicator="idhistory_narrow")
 
 
-idt.initialize_graph(organism_name='homo_sapiens', last_ensembl_release=80, return_test=True)
-
+idt.initialize_graph(organism_name="homo_sapiens", last_ensembl_release=82, return_test=True)
--- ddocs/usage.rst
+++ ddocs/usage.rst
@@ -8,7 +8,8 @@ Usage
 
 .. toctree::
 
-    _notebooks/initialization
+    _notebooks/prepare_new_external_yaml
+    _notebooks/initialization_graph
     _notebooks/results_preliminary_for_hlca
     _notebooks/random_data
     _notebooks/Untitled
